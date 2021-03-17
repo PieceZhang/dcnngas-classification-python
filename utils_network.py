3a38b002a50000662491c9bf9076822a5f7b746e
YuelinZhang <2239436414@qq.com>
2021-03-17 21:04:54 +0800

    add network_3a_arcface

--- utils_network.py
+++ utils_network.py
@@ -62,7 +62,7 @@ def network_1a_arcface():
                      activation='relu', strides=(1, 1))(bone)
     bone = kl.Flatten()(bone)
     bone = kl.Dense(units=1024, activation='relu', kernel_initializer='he_normal')(bone)
-    outputs = CosFace(n_classes=6, s=1, m=0.35)([bone, label])
+    outputs = ArcFace(n_classes=6, s=1, m=0.35)([bone, label])
     model = km.Model(inputs=[inputs, label], outputs=outputs)
     model.summary()
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
@@ -495,7 +495,7 @@ def network_1_m():
 
 
 # add network 3
-def network_3():
+def network_3(summary=False):
     """
     DIY network, use loadmat_3
     tricks1: 将大卷积分解成多个小卷积来减少计算量 3*3+3*3+2*2=5*5
@@ -541,16 +541,18 @@ def network_3():
                              branch['branch9'][0],branch['branch10'][0], branch['branch11'][0], branch['branch12'][0],
                              branch['branch13'][0], branch['branch14'][0], branch['branch15'][0],branch['branch16'][0]],
                      outputs=outputs)
-    model.summary()
+    if summary:
+        model.summary()
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
 
-def network_3a():
+def network_3a(summary=False):
     """
     DIY network, use loadmat_3
-    trick1：不使用maxpooling层
-    tricks2：多层FC（非线性划分）
+    trick1：不使用maxpooling层(10boards数据集已经经过预提取，无需池化)
+    trick2：多层FC（非线性划分）
+    trick3：elu(对输入变化或噪声更鲁棒)+relu激活函数组合
     :return:
     """
     def branch_create(scope):
@@ -559,13 +561,13 @@ def network_3a():
         bone = kl.BatchNormalization(1, name=scope + '/BN0')(inputs)
         bone = kl.Conv2D(filters=2, kernel_size=(3, 3), padding='same', strides=(1, 1), name=scope+'/Conv1')(bone)
         # bone = kl.BatchNormalization(1, name=scope+'/BN1')(bone)
-        bone = kl.Activation('relu', name=scope+'/relu1')(bone)
+        bone = kl.Activation('elu', name=scope+'/relu1')(bone)
         bone = kl.Conv2D(filters=4, kernel_size=(3, 3), padding='same', strides=(1, 1), name=scope+'/Conv2')(bone)
         # bone = kl.BatchNormalization(1, name=scope+'/BN2')(bone)
-        bone = kl.Activation('relu', name=scope+'/relu2')(bone)
+        bone = kl.Activation('elu', name=scope+'/relu2')(bone)
         bone = kl.Conv2D(filters=8, kernel_size=(3, 3), padding='same', strides=(1, 1), name=scope+'/Conv3')(bone)
         # bone = kl.BatchNormalization(1, name=scope+'/BN3')(bone)
-        bone = kl.Activation('relu', name=scope+'/relu3')(bone)
+        bone = kl.Activation('elu', name=scope+'/relu3')(bone)
         # bone = kl.MaxPool2D(pool_size=(3, 3), strides=(1, 1), padding='valid', name=scope)(bone)
         return inputs, bone
 
@@ -579,11 +581,7 @@ def network_3a():
                                                 branch['branch10'][1], branch['branch11'][1], branch['branch12'][1],
                                                 branch['branch13'][1], branch['branch14'][1], branch['branch15'][1],
                                                 branch['branch16'][1]])
-    # tree = kl.Conv2D(filters=8, kernel_size=(1, 3), padding='same', activation='relu', strides=(1, 1))(tree)
-    # tree = kl.Conv2D(filters=8, kernel_size=(1, 3), padding='same', activation='relu', strides=(1, 1))(tree)
-    # tree = kl.Conv2D(filters=8, kernel_size=(1, 2), padding='same', activation='relu', strides=(1, 1))(tree)
     tree = kl.Flatten()(tree)
-    tree = kl.Dense(units=200, activation='relu')(tree)
     tree = kl.Dense(units=200, activation='relu')(tree)
     tree = kl.Dense(units=200, activation='relu')(tree)
     outputs = kl.Dense(units=6, activation='softmax')(tree)
@@ -592,7 +590,55 @@ def network_3a():
                              branch['branch9'][0],branch['branch10'][0], branch['branch11'][0], branch['branch12'][0],
                              branch['branch13'][0], branch['branch14'][0], branch['branch15'][0],branch['branch16'][0]],
                      outputs=outputs)
-    model.summary()
+    if summary:
+        model.summary()
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+def network_3a_arcface(summary=False):
+    """
+    DIY network, use loadmat_3
+    :return:
+    """
+    def branch_create(scope):
+        assert type(scope) == str, 'TypeError: scope should be a str'
+        inputs = kl.Input(shape=(3, 6, 1), name=scope+'/Input')
+        bone = kl.BatchNormalization(1, name=scope + '/BN0')(inputs)
+        bone = kl.Conv2D(filters=2, kernel_size=(3, 3), padding='same', strides=(1, 1), name=scope+'/Conv1')(bone)
+        # bone = kl.BatchNormalization(1, name=scope+'/BN1')(bone)
+        bone = kl.Activation('elu', name=scope+'/relu1')(bone)
+        bone = kl.Conv2D(filters=4, kernel_size=(3, 3), padding='same', strides=(1, 1), name=scope+'/Conv2')(bone)
+        # bone = kl.BatchNormalization(1, name=scope+'/BN2')(bone)
+        bone = kl.Activation('elu', name=scope+'/relu2')(bone)
+        bone = kl.Conv2D(filters=8, kernel_size=(3, 3), padding='same', strides=(1, 1), name=scope+'/Conv3')(bone)
+        # bone = kl.BatchNormalization(1, name=scope+'/BN3')(bone)
+        bone = kl.Activation('elu', name=scope+'/relu3')(bone)
+        # bone = kl.MaxPool2D(pool_size=(3, 3), strides=(1, 1), padding='valid', name=scope)(bone)
+        return inputs, bone
+
+    label = kl.Input(shape=(6,))
+    branch = {}
+    for branch_num in range(1, 17):
+        branch_name = 'branch{}'.format(branch_num)
+        branch[branch_name] = branch_create(branch_name)
+    tree = kl.Concatenate(axis=1, name='tree')([branch['branch1'][1], branch['branch2'][1], branch['branch3'][1],
+                                                branch['branch4'][1], branch['branch5'][1], branch['branch6'][1],
+                                                branch['branch7'][1], branch['branch8'][1], branch['branch9'][1],
+                                                branch['branch10'][1], branch['branch11'][1], branch['branch12'][1],
+                                                branch['branch13'][1], branch['branch14'][1], branch['branch15'][1],
+                                                branch['branch16'][1]])
+    tree = kl.Flatten()(tree)
+    tree = kl.Dense(units=200, activation='relu')(tree)
+    tree = kl.Dense(units=200, activation='relu')(tree)
+    outputs = ArcFace(n_classes=6, s=5, m=0.05)([tree, label])
+    model = km.Model(inputs=[branch['branch1'][0], branch['branch2'][0], branch['branch3'][0],branch['branch4'][0],
+                             branch['branch5'][0], branch['branch6'][0],branch['branch7'][0], branch['branch8'][0],
+                             branch['branch9'][0],branch['branch10'][0], branch['branch11'][0], branch['branch12'][0],
+                             branch['branch13'][0], branch['branch14'][0], branch['branch15'][0],branch['branch16'][0], label],
+                     outputs=outputs)
+    if summary:
+        model.summary()
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
